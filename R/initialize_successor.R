5c7e244e8be2db2a4665c4ce7d1b719774212967
Jae-Young Son <48289808+psychNerdJae@users.noreply.github.com>
2022-07-26 21:06:03 -0400

    Update initialize_successor.R

--- R/initialize_successor.R
+++ R/initialize_successor.R
@@ -1,35 +1,36 @@
 #' @title Initialize successor values
-#' @name initialize_successor
 #'
 #' @description
 #' The successor counts must be initialized to some value. All things equal,
 #' the value `0` tends to be pretty good.
 #'
-#' @param input_tidygraph A `tbl_graph` representation from `tidygraph`.
-#'     This function will not work with other kinds of network representations,
-#'     e.g. from `igraph`. Make sure that your input graph is either directed
-#'     or undirected, as you'd like it.
-#' @param initialize_with A scalar. Defaults to `0`.
+#' @param input Formatted either as `tidygraph::tbl_graph`, OR a scalar with
+#' the number of nodes. This function will not work with other kinds of network
+#' representations, e.g. from `igraph`. Make sure that your input graph is
+#' either directed or undirected, as you'd like it.
+#' @param initialize_with Scalar, defaults to `0`.
 #' @return A square NxN matrix, where N=number of nodes.
 #'
 #' @export
 #'
 #' @examples
 #' `%>%` <- magrittr::`%>%`
-#' successr::karate %>%
-#'     tidygraph::as_tbl_graph(directed = F) %>%
-#'     initialize_successor()
+#' tidygraph::tbl_graph(edges = successr::karate, directed = F) %>%
+#'   initialize_successor()
 #'
-initialize_successor <- function(input_tidygraph, initialize_with = 0) {
+initialize_successor <- function(input, initialize_with = 0) {
 
-  input_matrix <- input_tidygraph %>%
-    graph_to_adjlist() %>%
-    adjlist_to_matrix()
+  if (is.numeric(input)) {
+    if (length(input) != 1) {
+      stop("If providing a scalar, specify the total number of nodes.")
+    }
+    square_size <- input
+  } else if (any(class(input) == "tbl_graph")) {
+    square_size <- dim(convert_graph_to_matrix(input))[1]
+  } else {
+    stop("Input must be a scalar or `tidygraph::tbl_graph`.")
+  }
 
-  square_size <- dim(input_matrix)[1]
-
-  output <- matrix(initialize_with, square_size, square_size)
-
-  return (output)
+  return (matrix(initialize_with, square_size, square_size))
 }
 
